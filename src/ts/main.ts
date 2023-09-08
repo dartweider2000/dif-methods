8583f1237711bb356458661e8a67201279e2248d
dartweider2000 <nikita.ivanov574477333@gmail.com>
2023-09-08 12:21:34 +0300

    change submitHandler

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', loadHandler);
 
 function loadHandler(e: Event){
-   
+
    const initCheckbox = () => {
 
       const checkbox = document.querySelector('.checkbox')!;
@@ -18,15 +18,23 @@ function loadHandler(e: Event){
 
    const form: HTMLFormElement = document.querySelector('.form')!;
 
-   const tMaxInput = form.elements.namedItem('tMax') as HTMLInputElement;
+   const xMaxInput = form.elements.namedItem('xMax') as HTMLInputElement;
    const printCheckbox = form.elements.namedItem('print') as HTMLInputElement;
+   const tayInput = form.elements.namedItem('tay') as HTMLInputElement;
+   const xZeroInput = form.elements.namedItem('x_0') as HTMLInputElement;
+   const yZeroInput = form.elements.namedItem('y_0') as HTMLInputElement;
 
    // tMaxInput.focus();
 
    form.addEventListener('submit', e => {
       e.preventDefault();
 
-      const tMaxInputValue: number = +tMaxInput.value;
+      const xMaxInputValue: number = +xMaxInput.value;
+      const tayInputValue: number = +tayInput.value;
+      const xZeroInputValue: number = +xZeroInput.value;
+      const yZeroInputValue: number = +yZeroInput.value;
       const printValue: boolean = printCheckbox.checked;
+
+      console.log(xMaxInputValue, tayInputValue, xZeroInputValue, yZeroInputValue, printValue);
    });
 }
